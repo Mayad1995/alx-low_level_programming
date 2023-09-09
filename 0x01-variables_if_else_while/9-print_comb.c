2e76ee309fc6bea39bdd4b2eb9a6b727a92ac5b2
Mayad1995 <mayadhisham95@gmail.com>
2023-09-08 18:54:10 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print 0, 1, - 9
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+{
+putchar('0' + digit);
+
+if (digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return 0;
+}
