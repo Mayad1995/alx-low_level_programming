15e3127db878a550f2a7c463b326bc691fc89b1c
Mayad1995 <mayadhisham95@gmail.com>
2023-09-08 19:08:23 -0700

    a

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,16 +9,10 @@
 */
 int main(void)
 {
-int digit = 0;
-for (digit = 0; digit < 10; digit++)
+int digit;
+for (digit = 0; digit <= 9; digit++)
 {
 putchar('0' + digit);
-
-if (digit != 9)
-{
-putchar(',');
-putchar(' ');
-}
 }
 putchar('\n');
 
