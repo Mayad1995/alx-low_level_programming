c99c6f557f5344f796b6a7d015374f0fa1b57014
Mayad1995 <mayadhisham95@gmail.com>
2023-09-13 19:32:15 -0700

    a

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: prints the first 50 Fibonacci numbers 
+ * Description: prints the first 50 Fibonacci numbers
  *              starting with 1 and 2 followed by a new line
  *
  * Return: Always 0 (Success)
